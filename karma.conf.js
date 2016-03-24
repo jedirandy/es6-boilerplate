be6606e7e11e5bd6ac255b41202d01130710396d
jedirandy <jedirandy@gmail.com>
2016-03-24 23:32:52 +0100

    remove babel polyfill

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'node_modules/babel-polyfill/dist/polyfill.js',
-        'src/**/*.js',
         'test/**/*.js'
     ],
 
@@ -29,7 +27,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/**/*.js': ['browserify'],
         'test/**/*.js': ['browserify']
     },
 
